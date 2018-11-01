1e6849bef05e6a023d3d1a55ff75eeb4f79d1d8d
Andrea Visentin <andrea.visentin@insight-centre.org>
2018-11-01 12:17:31 +0000

    multifollower

--- instance.py
+++ instance.py
@@ -145,3 +145,125 @@ class Instance:
         file.write(line)
 
         file.close()
+
+
+class InstanceMultifollower:
+    m = 10
+    n = 10
+    N = n * m
+    K = 1000
+    a = 2 * (n - 2) * (5 * m - 4) + 3 * m - 2
+    c = 10
+    d = 10
+    r = 5
+    r0 = 20
+    arcs = []
+    paths = []
+
+    def set_r0(self, r0):
+        self.r0 = r0
+
+    def set_args(self, m, n, k, c, d, r, r0):
+        self.m = m
+        self.n = n
+        self.K = k
+        self.c = c
+        self.d = d
+        self.r = r
+        self.r0 = r0
+        self.N = n * m
+        self.a = 2 * (n - 2) * (5 * m - 4) + 3 * m - 2
+
+    def node_nr(self, x, y):
+        return self.m * (y - 1) + x + 1
+
+    def arcs_creator(self, seed, npaths):
+        self.paths = random.sample(range(1, self.N), npaths * 2)
+        self.arcs = []
+        random.seed(seed)
+        for j in range(1, self.m + 1):
+            for i in range(1, self.n + 1):
+                if i + 1 <= self.n and j != 1 and j != self.m:
+                    arc = Arc()
+                    arc.start = self.node_nr(i, j)
+                    arc.end = self.node_nr(i + 1, j)
+                    arc.length = random.randint(1, self.c)
+                    arc.disruption = random.randint(1, self.d)
+                    arc.cost = random.randint(1, self.r)
+                    self.arcs.append(arc)
+                if i - 1 > 0 and j != 1 and j != self.m:
+                    arc = Arc()
+                    arc.start = self.node_nr(i, j)
+                    arc.end = self.node_nr(i - 1, j)
+                    arc.length = random.randint(1, self.c)
+                    arc.disruption = random.randint(1, self.d)
+                    arc.cost = random.randint(1, self.r)
+                    self.arcs.append(arc)
+                if j + 1 <= self.m:
+                    arc = Arc()
+                    arc.start = self.node_nr(i, j)
+                    arc.end = self.node_nr(i, j + 1)
+                    arc.length = random.randint(1, self.c)
+                    arc.disruption = random.randint(1, self.d)
+                    arc.cost = random.randint(1, self.r)
+                    self.arcs.append(arc)
+                if j + 1 <= self.m and i + 1 <= self.n:
+                    arc = Arc()
+                    arc.start = self.node_nr(i, j)
+                    arc.end = self.node_nr(i + 1, j + 1)
+                    arc.length = random.randint(1, self.c)
+                    arc.disruption = random.randint(1, self.d)
+                    arc.cost = random.randint(1, self.r)
+                    self.arcs.append(arc)
+                if j + 1 <= self.m and i - 1 > 0:
+                    arc = Arc()
+                    arc.start = self.node_nr(i, j)
+                    arc.end = self.node_nr(i - 1, j + 1)
+                    arc.length = random.randint(1, self.c)
+                    arc.disruption = random.randint(1, self.d)
+                    arc.cost = random.randint(1, self.r)
+                    self.arcs.append(arc)
+
+        return self.arcs
+
+    def write_dzn_file(self, name):
+        file = open(name, "w+")
+        file.write("N = " + str(self.N) + ";\n")
+        file.write("K = " + str(self.K) + ";\n")
+        file.write("M = " + str(self.a) + ";\n")
+        file.write("R0 = " + str(self.r0) + ";\n")
+        for i in range(0, len(self.paths)/2):
+            file.write("Start_" + chr(97 + i) + " = " + self.paths[2*i] + ";\n")
+            file.write("End_" + chr(97 + i) + " = " + self.paths[2*i+1] + ";\n")
+
+        line = "Edge_Start =  ["
+        for i in self.arcs[:-1]:
+            line = line + str(i.start) + " ,"
+        line = line + str(self.arcs[-1].start) + "];\n"
+        file.write(line)
+
+        line = "Edge_End =  ["
+        for i in self.arcs[:-1]:
+            line = line + str(i.end) + " ,"
+        line = line + str(self.arcs[-1].end) + "];\n"
+        file.write(line)
+
+        line = "L =  ["
+        for i in self.arcs[:-1]:
+            line = line + str(i.length) + " ,"
+        line = line + str(self.arcs[-1].length) + "];\n"
+        file.write(line)
+
+        line = "D =  ["
+        for i in self.arcs[:-1]:
+            line = line + str(i.disruption) + " ,"
+        line = line + str(self.arcs[-1].disruption) + "];\n"
+        file.write(line)
+
+        line = "R =  ["
+        for i in self.arcs[:-1]:
+            line = line + str(i.cost) + " ,"
+        line = line + str(self.arcs[-1].cost) + "];\n"
+        file.write(line)
+
+        file.close()
